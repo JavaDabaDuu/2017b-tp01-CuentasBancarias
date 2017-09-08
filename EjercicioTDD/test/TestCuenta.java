96657b9fe1eab8030e77e5e041d2ca5ed626e577
RamosMicaela <ramosmicaela@outlook.es>
2017-09-08 20:42:16 -0300

    ROJO - El monto no puede ser negativo

--- EjercicioTDD/test/TestCuenta.java
+++ EjercicioTDD/test/TestCuenta.java
@@ -7,10 +7,12 @@ import cuentaBancaria.CuentaBancaria;
 
 public class TestCuenta {
 	
-	@Test
+	@Test(expected = IllegalArgumentException.class)
 	public void haceTransferencia() {
-		CuentaBancaria obj = new CuentaBancaria();
-		obj.realizarTransferencia(5100, obj);
-		Assert.assertEquals(5100, obj.getMonto(),0.001);
+		CuentaBancaria obj1 = new CuentaBancaria();
+		CuentaBancaria obj2 = new CuentaBancaria();
+		
+		obj1.realizarTransferencia(-5100, obj2);
+		Assert.assertEquals(-5100, obj2.getMonto(),0.001);
 	}
 }

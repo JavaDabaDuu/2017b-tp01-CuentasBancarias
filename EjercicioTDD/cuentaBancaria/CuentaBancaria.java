adfa53ba8a48031c08a5de4f544cb56492f66ffa
RamosMicaela <ramosmicaela@outlook.es>
2017-09-08 22:59:28 -0300

    VERDE- Se logra que test encuentra cuentas iguales funcione bien.

--- EjercicioTDD/cuentaBancaria/CuentaBancaria.java
+++ EjercicioTDD/cuentaBancaria/CuentaBancaria.java
@@ -2,39 +2,44 @@ package cuentaBancaria;
 
 public class CuentaBancaria {
 	private double monto;
-	private long numCuenta;
-	
-	public CuentaBancaria (double monto) {
+	private long nroCuenta;
+
+	public CuentaBancaria(double monto, long nroCuenta) {
+		this.monto = monto;
+		this.nroCuenta = nroCuenta;
+	}
+
+	public CuentaBancaria(double monto) {
 		this.monto = monto;
 	}
-	
-	public void realizarTransferencia (double monto,CuentaBancaria cuenta2) {		
+
+	public void realizarTransferencia(double monto, CuentaBancaria cuenta2) {
 		if (monto < 0) {
 			throw new IllegalArgumentException();
 		}
-		
-		if(this.esMontoSuficiente(monto)) {
+
+		if (this.esMontoSuficiente(monto)) {
 			cuenta2.monto += monto;
 			this.monto -= monto;
 		}
 	}
-	
+
 	public boolean esMontoSuficiente(double monto) {
-		return monto<=this.monto;	
+		return monto <= this.monto;
 	}
-	
+
 	public boolean distintasCuentas(CuentaBancaria cuenta2) {
-		if(cuenta2.getNroCuenta() != this.getNroCuenta())
+		if (cuenta2.getNroCuenta() == this.getNroCuenta())
 			return true;
-		
+
 		return false;
 	}
-	
+
 	public double getMonto() {
 		return this.monto;
 	}
-	
+
 	public long getNroCuenta() {
-		return this.numCuenta;
+		return this.nroCuenta;
 	}
 }
